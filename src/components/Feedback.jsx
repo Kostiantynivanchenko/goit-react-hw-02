d5ae002e5d4f24fc0d0ddc4eba806a487d086a6b
kostiantyn_ivanchenko <kokos2702@gmail.com>
2025-01-10 23:29:53 +0100

    sss

---
+++ src/components/Feedback.jsx
@@ -0,0 +1,17 @@
+import style from "./Feedback.module.css";
+
+export default function Feedback({
+  typeFeedback,
+  totalFeedback,
+  positiveFeedback,
+}) {
+  return (
+    <ul className={style.list}>
+      <li>Good: {typeFeedback.good}</li>
+      <li>Neutral: {typeFeedback.neutral}</li>
+      <li>Bad: {typeFeedback.bad}</li>
+      <li>Total: {totalFeedback}</li>
+      <li>Positive: {positiveFeedback}%</li>
+    </ul>
+  );
+}
---
+++ src/components/Options.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import styles from "./Options.module.css";
+
+export default function Options({
+  totalFeedback,
+  typeFeedback,
+  updateFeedback,
+  resetFeedback,
+}) {
+  return (
+    <div className={styles.button_container}>
+      <button
+        onClick={() => {
+          updateFeedback("good");
+        }}
+        className={styles.button}
+      >
+        Good
+      </button>
+      <button
+        onClick={() => {
+          updateFeedback("neutral");
+        }}
+        className={styles.button}
+      >
+        Neutral
+      </button>
+      <button
+        onClick={() => {
+          updateFeedback("bad");
+        }}
+        className={styles.button}
+      >
+        Bad
+      </button>
+      {totalFeedback > 0 && (
+        <button onClick={resetFeedback} className={styles.button}>
+          Reset
+        </button>
+      )}
+    </div>
+  );
+}
